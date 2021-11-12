e897f86bd7eb8b844977a30cfbfa5fa7fa1389ec
5t3ph <thinkdobecreate@gmail.com>
2021-11-12 16:54:23 -0600

    Update site to handle for Season 2!

--- .eleventy.js
+++ .eleventy.js
@@ -12,13 +12,11 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addWatchTarget("./src/sass/");
 
-  // TEMPORARY
-  eleventyConfig.addPassthroughCopy("./src/stings");
-
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/fonts");
   eleventyConfig.addPassthroughCopy("./src/img");
   eleventyConfig.addPassthroughCopy("./src/favicon.png");
+  eleventyConfig.addPassthroughCopy("./src/_redirects");
 
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
